8f4f3b823bc4d14173a9a4cfaa28fde980543a7e
jaycobchin <chinyanjie@gmail.com>
2026-01-29 14:34:59 +0800

    fix favicon on safari browser

--- app/layout.js
+++ app/layout.js
@@ -44,6 +44,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
